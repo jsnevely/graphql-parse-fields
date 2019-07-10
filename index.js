fd08f8c00171171728688f475244f935095131a5
Joe Snevely <jsnevely@knowland.com>
2019-07-10 14:46:58 -0400

    adding parseArgs

--- index.js
+++ index.js
@@ -11,21 +11,24 @@ module.exports = parseFields
  * 1)
  * @param {Object} info - graphql resolve info
  * @param {Boolean} [keepRoot] default: true
+ * @param {Boolean} [parseArgs] default: false
  * @return {Object} fieldTree
  * 2)
  * @param {Array} asts - ast array
  * @param {Object} [fragments] - optional fragment map
  * @param {Object} [fieldTree] - optional initial field tree
+ * @param {Object} [parseArgs] default:false
  * @return {Object} fieldTree
  */
 function parseFields (/* dynamic */) {
   var tree
   var info = arguments[0]
   var keepRoot = arguments[1]
+  var parseArgs = arguments[2]
   var fieldNodes = info && (info.fieldASTs || info.fieldNodes)
   if (fieldNodes) {
     // (info, keepRoot)
-    tree = fieldTreeFromAST(fieldNodes, info.fragments)
+    tree = fieldTreeFromAST(fieldNodes, info.fragments, {}, parseArgs)
     if (!keepRoot) {
       var key = firstKey(tree)
       tree = tree[key]
@@ -37,7 +40,7 @@ function parseFields (/* dynamic */) {
   return tree
 }
 
-function fieldTreeFromAST (asts, fragments, init) {
+function fieldTreeFromAST (asts, fragments, init, parseArgs) {
   init = init || {}
   fragments = fragments || {}
   asts = castArr(asts)
@@ -48,17 +51,25 @@ function fieldTreeFromAST (asts, fragments, init) {
     if (kind === 'Field') {
       if (val.selectionSet) {
         tree[name] = tree[name] || {}
-        fieldTreeFromAST(val.selectionSet.selections, fragments, tree[name])
+        fieldTreeFromAST(val.selectionSet.selections, fragments, tree[name], parseArgs)
       } else {
         tree[name] = true
+      }
+      if(parseArgs && val.arguments && val.arguments.length > 0) {
+        tree[name].args = val.arguments.reduce(function(args, a){
+          if(a.value.value !== undefined) {
+            args[a.name.value] = a.value.value
+            return args;
+          }
+        }, {})
       }
     } else if (kind === 'FragmentSpread') {
       fragment = fragments[name]
       assert(fragment, 'unknown fragment "' + name + '"')
-      fieldTreeFromAST(fragment.selectionSet.selections, fragments, tree)
+      fieldTreeFromAST(fragment.selectionSet.selections, fragments, tree, parseArgs)
     } else if (kind === 'InlineFragment') {
       fragment = val
-      fieldTreeFromAST(fragment.selectionSet.selections, fragments, tree)
+      fieldTreeFromAST(fragment.selectionSet.selections, fragments, tree, parseArgs)
     } // else ignore
     return tree
   }, init)
